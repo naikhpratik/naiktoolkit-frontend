9d9e460bac26d1ee24e544f0f0b7c2004826650a
Pratik Naik <naikhpratik@gmail.com>
2024-09-13 15:37:24 -0400

    v2

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import Header from './Header';
 import HeroSection from './HeroSection';
 import ProductGrid from './ProductGrid';
 
