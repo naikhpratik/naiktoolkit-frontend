46ea1918687705337dc6bda202d83073fec42707
Pratik Naik <naikhpratik@gmail.com>
2024-09-14 16:00:29 -0400

    updated code to handle login correctly

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,28 +1,15 @@
-// import axios from 'axios';
-// import dotenv from 'dotenv';
-// dotenv.config({ path: './.env' });
+import axios from 'axios';
 
-// console.log('Aaya');
-// const API = axios.create({
-//   baseURL: process.env.REACT_APP_API_URL,  // Add the API URL in .env
-// });
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,  // Ensure API URL is correct
+});
 
-// // Log the baseURL
-// console.log('Base URL:', API.defaults.baseURL);
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
-// // Optionally add request interceptors to add authorization token to requests
-// API.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('token');
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-// export default API;
-// Assuming there's an api object or function
-const api = {
-  // ... api methods
-};
-
-export default api;
+export default API;
